27aff166a88bf5b1a557005d5d142690835e1c98
Roberto Vazquez <rvazquezbasulto@gmail.com>
2025-06-16 15:43:28 -0400

    add state

--- Instance.tf
+++ Instance.tf
@@ -10,3 +10,8 @@ resource "aws_instance" "web" {
     Project = "Dove"
   }
 }
+
+resource "aws_ec2_instance_state" "web-state" {
+    instance_id = aws_instance.web.id
+    state       = "running"
+}
